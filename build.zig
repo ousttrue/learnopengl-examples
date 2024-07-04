f449279d6b9c16af5b69ef0f6eed71eb4d3bf811
ousttrue <ousttrue@gmail.com>
2024-07-04 17:39:15 +0900

    zig sokol

---
+++ build.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const dep_sokol = b.dependency("sokol", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "learnopengl-examples",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("sokol", dep_sokol.module("sokol"));
+    b.installArtifact(exe);
+}
