3e56dd9e035a4e77448c1dd6ae88ed53f650a703
ousttrue <ousttrue@gmail.com>
2024-08-18 02:01:35 +0900

    docusaurus

---
+++ docs/screenshot.ts
@@ -0,0 +1,26 @@
+const pw = require('playwright');
+const examples = require('./src/components/HomepageFeatures/examples.json');
+
+(async () => {
+  const browser = await pw.chromium.launch(); // or 'chromium', 'firefox'
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  // page.setDefaultTimeout(30000 * 30);
+  page.setViewportSize({ "width": 400, "height": 300 });
+
+  let i=1;
+  for (const group of examples) {
+    for (const article of group.list) {
+      for (const name of article.sections) {
+        const url = `http://localhost:3000/learnopengl_examples/wasm/${name}.html`
+        console.log(i, url);
+        await page.goto(url);
+        await page.waitForLoadState('networkidle')
+        await page.screenshot({ path: `static/wasm/${name}.jpg` });
+        ++i;
+      }
+    }
+  }
+
+  await browser.close();
+})();
---
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,66 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+import LearnOpenGL from './examples.json';
+import Link from '@docusaurus/Link';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+
+function Item({ name }: { name: string }) {
+  return (
+
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{name}</Heading>
+        <p>
+          <Link
+            target="_blank"
+            to={useBaseUrl(`/wasm/${name}.html`)}>
+            <ThemedImage
+              sources={{
+                light: useBaseUrl(`/wasm/${name}.jpg`),
+                dark: useBaseUrl(`/wasm/${name}.jpg`),
+              }} />
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Feature({ title, url, sections }) {
+  return (
+    <>
+      {sections.map((name, idx) => (
+        <Item key={idx} name={name} />
+      ))
+      }
+    </>
+  );
+}
+
+function Group({ name, list }) {
+  return (
+    <>
+      {list.map((props, idx) => (
+        <Feature key={idx} {...props} />
+      ))
+      }
+    </>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {LearnOpenGL.map((props, idx) => (
+            <Group key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
